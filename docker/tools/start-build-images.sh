cfa77c9b99c6d6005a140b99e0e184c4ce252a2c
wanghiato71 <haetao@outlook.com>
2019-12-12 09:22:51 +0800

    modify shell script

--- docker/tools/start-build-images.sh
+++ docker/tools/start-build-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+resd jobName
 cd docker
 sudo cp ../../${jobName}/sharding-distribution/sharding-proxy-distribution/target/*.tar.gz  ./sharding-proxy-bin.tar.gz
 sudo docker-compose up -d
